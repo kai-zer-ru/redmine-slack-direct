188115775aeaebe65d2beaf7ac899e56baa4debb
kaizer <kaizer@kai-zer.ru>
2017-08-01 11:25:27 +0800

    ReadMe

--- init.rb
+++ init.rb
@@ -3,12 +3,12 @@ require 'redmine'
 require_dependency 'redmine_slack/listener'
 
 Redmine::Plugin.register :redmine_slack do
-	name 'Redmine Slack'
-	author 'Samuel Cormier-Iijima'
-	url 'https://github.com/sciyoshi/redmine-slack'
-	author_url 'http://www.sciyoshi.com'
+	name 'Redmine Slack Direct'
+	author 'Kaizer'
+	url 'https://github.com/kaizer666/redmine-slack-direct'
+	author_url 'https://blog.kai-zer.ru'
 	description 'Slack chat integration'
-	version '0.2'
+	version '0.1'
 
 	requires_redmine :version_or_higher => '0.8.0'
 
@@ -18,7 +18,7 @@ Redmine::Plugin.register :redmine_slack do
 			'channel' => nil,
 			'icon' => 'https://raw.github.com/sciyoshi/redmine-slack/gh-pages/icon.png',
 			'username' => 'redmine',
-			'display_watchers' => 'no'
+			'display_watchers' => 'yes'
 		},
 		:partial => 'settings/slack_settings'
 end
